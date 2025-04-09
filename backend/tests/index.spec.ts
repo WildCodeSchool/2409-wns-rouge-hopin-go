cc66f8ecf05a860da2f3b22b9b478cc2bf7f7a2b
Adrien Davy <adriendavyfx@gmail.com>
2025-04-09 15:19:28 +0200

    test backend & adding script to test it

---
+++ backend/tests/index.spec.ts
@@ -0,0 +1,46 @@
+import { getSchema } from "../src/schema";
+import { ApolloServer, BaseContext } from "@apollo/server";
+import { datasource } from "../src/datasource";
+import { DataSource } from "typeorm";
+import { UsersResolverTest } from "./resolvers/UsersResolver";
+
+export type TestArgsType = {
+  server: ApolloServer<BaseContext>;
+  datasource: DataSource;
+  data: any;
+};
+
+const testArgs: TestArgsType = {
+  server: {} as ApolloServer<BaseContext>,
+  datasource: {} as DataSource,
+  data: {},
+};
+
+export function assert(expr: unknown, msg?: string): asserts expr {
+  if (!expr) throw new Error(msg);
+}
+
+beforeAll(async () => {
+  await datasource.initialize();
+  try {
+    const entities = datasource.entityMetadatas;
+    const tableNames = entities
+      .map((entity) => `"${entity.tableName}"`)
+      .join(", ");
+    await datasource.query(`TRUNCATE ${tableNames} CASCADE;`);
+  } catch (error) {
+    throw new Error(`ERROR: Cleaning test database: ${error}`);
+  }
+
+  const schema = await getSchema();
+
+  const testServer = new ApolloServer({
+    schema,
+  });
+  testArgs.datasource = datasource;
+  testArgs.server = testServer;
+});
+
+describe("users resolver", () => {
+  UsersResolverTest(testArgs);
+});
