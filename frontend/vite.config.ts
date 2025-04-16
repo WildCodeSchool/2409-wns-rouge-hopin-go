b40b8285ab6684c71c88f5a3b5ec9b1ab73cd762
Adrien Davy <adriendavyfx@gmail.com>
2025-04-16 16:04:12 +0200

    Ajout de la configuration Nginx et des services dans les fichiers Docker Compose pour le déploiement en production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,17 +8,19 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
-    proxy: {
-      "/api": {
-        target: "http://back:5000", // or http://localhost:5000 if you are running the front locally with npm run dev
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "http://back:5000", // or http://localhost:5000 if you are running the front locally with npm run dev
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    // },
+    allowedHosts: ["front"],
+    hmr: { path: "hmr" },
   },
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./src/setup.ts",
-  },
+  // test: {
+  //   environment: "jsdom",
+  //   globals: true,
+  //   setupFiles: "./src/setup.ts",
+  // },
 });
