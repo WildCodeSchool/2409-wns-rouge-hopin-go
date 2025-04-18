7ce6e63b096abbb96c91fa6ec330a1c5deaa6d03
Adrien Davy <adriendavyfx@gmail.com>
2025-04-18 16:39:40 +0200

    Refactor code structure for improved readability and maintainability

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import "react-toastify/dist/ReactToastify.css";
+import Logo from "../public/logo.png";
 
 const App = () => {
   return (
     <>
       <div className="flex flex-col min-h-screen">
         <header>
+          <img
+            src={Logo}
+            alt="Logo"
+            className="absolute top-0 left-0 w-16 h-16 m-2"
+          />
           <NavBar />
         </header>
         <main className="h-screen">
