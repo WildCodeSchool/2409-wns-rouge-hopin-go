77cfd63770a160005010e4343bd449c642e034e8
Aurélie <aurelie.demure.ad@gmail.com>
2025-07-17 11:53:32 +0200

    fix card modal on archived ride

--- frontend/src/components/PassengersButtonWithModal.tsx
+++ frontend/src/components/PassengersButtonWithModal.tsx
@@ -10,6 +10,7 @@ import { useQuery } from "@apollo/client";
 import { queryPassengersByRide } from "../api/PassengersByRide";
 import { useState } from "react";
 import useRide from "../context/Rides/useRide";
+import { isAfter, parseISO } from "date-fns";
 
 type PassengersButtonWithModalProps = {
   variant: VariantType;
@@ -21,6 +22,11 @@ const PassengersButtonWithModal = ({
   const ride = useRide();
   const { isOpen, visible, toggleModal: toggleParentModal } = useModal();
   const { isXl } = useWindowSize();
+  console.log("Ride at:", ride.departure_at);
+
+  const departureDate = parseISO(ride.departure_at);
+  const now = new Date();
+  const isFuture = isAfter(departureDate, now);
 
   const [info, setInfo] = useState(false);
 
@@ -45,7 +51,7 @@ const PassengersButtonWithModal = ({
     <div>
       <div className="absolute right-[170px] flex gap-2 items-center z-10 p-2 text-sm lg:text-base text-textLight font-semibold">
         <div className="relative">
-          {waitingPassengers && waitingPassengers?.length > 0 && (
+          {waitingPassengers && waitingPassengers?.length > 0 && isFuture && (
             <>
               <span
                 className="absolute rounded-full -right-[2px] -top-[2px] w-3 h-3 bg-refused animate-ping
@@ -70,15 +76,18 @@ const PassengersButtonWithModal = ({
             label={isXl ? "Passagers" : ""}
             variant="secondary"
           />
-          {info && waitingPassengers && waitingPassengers.length > 0 && (
-            <div className="absolute bottom-full left-full bg-refused text-white overflow-hidden p-2 w-40 rounded-lg shadow-lg z-50">
-              <p className="text-xs flex items-center justify-center gap-1">
-                {waitingPassengers.length} passager
-                {waitingPassengers.length > 1 ? "s" : ""} en attente
-              </p>
-            </div>
-          )}
-          {info && waitingPassengers.length === 0 && (
+          {info &&
+            waitingPassengers &&
+            waitingPassengers.length > 0 &&
+            isFuture && (
+              <div className="absolute bottom-full left-full bg-refused text-white overflow-hidden p-2 w-40 rounded-lg shadow-lg z-50">
+                <p className="text-xs flex items-center justify-center gap-1">
+                  {waitingPassengers.length} passager
+                  {waitingPassengers.length > 1 ? "s" : ""} en attente
+                </p>
+              </div>
+            )}
+          {info && waitingPassengers.length === 0 && isFuture && (
             <div className="absolute bottom-full left-full bg-refused text-white overflow-hidden p-2 w-40 rounded-lg shadow-lg z-50">
               <p className="text-xs flex items-center justify-center gap-1">
                 Aucun passager en attente
