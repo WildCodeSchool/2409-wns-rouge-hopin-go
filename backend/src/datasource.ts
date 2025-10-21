322429b0186bff0842dacbd17d449c7a8a66a6f4
Aurélie <aurelie.demure.ad@gmail.com>
2025-10-21 14:52:26 +0200

    feat: create initial migration

--- backend/src/datasource.ts
+++ backend/src/datasource.ts
@@ -1,17 +1,19 @@
 import { DataSource } from "typeorm";
 
-// const isTest = process.env.NODE_ENV === "testing";
+const syncMode = process.env.NODE_ENV === "dev";
 
 export const datasource = new DataSource({
   type: "postgres",
   host: process.env.POSTGRES_HOST,
-  //  || (isTest ? "localhost" : "db"),
   port: parseInt(process.env.POSTGRES_PORT || "5432"),
-  // || (isTest ? "5434" : "5432")),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   entities: ["./src/entities/*.ts"],
-  synchronize: true,
+  // Automatic synchronization only in dev
+  synchronize: syncMode,
+  migrations: ["./migrations/*.ts"],
+  // runs migrations automatically in prod and staging
+  migrationsRun: !syncMode,
   logging: true,
 });
